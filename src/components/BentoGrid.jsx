ddca5610983ff7e7c158a3ff802232dd14a3d417
Nicolas Giraldo Arenas <nicolas.giraldo1@utp.edu.co>
2024-03-27 13:10:58 -0500

    iconos bento linkedin y github

--- src/components/BentoGrid.jsx
+++ src/components/BentoGrid.jsx
@@ -84,13 +84,18 @@ export const BentoGrid = () => {
                     <a
                         href="https://www.linkedin.com/in/nicolas-giraldo05"
                         target="_blank"
-                        className="flex flex-row text-black items-center gap-1"
+                        className="flex flex-row text-black items-center"
                         rel="noreferrer"
                     >
-                        <FaLinkedin
-                            className="text-5xl md:text-6xl"
-                            style={{ color: "black" }}
-                        />
+                        <button className="group relative inline-flex h-15 items-center justify-center overflow-hidden rounded-md bg-neutral-950 font-medium text-neutral-200 duration-500">
+                            <div className="translate-y-0 opacity-100 transition group-hover:-translate-y-[150%] group-hover:opacity-0">
+                                <FaLinkedin
+                                    className="text-5xl md:text-6xl"
+                                />
+                            </div>
+                            <div className="absolute translate-y-[150%] opacity-0 transition group-hover:translate-y-0 group-hover:opacity-100"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6"><path d="M7.5 2C7.77614 2 8 2.22386 8 2.5L8 11.2929L11.1464 8.14645C11.3417 7.95118 11.6583 7.95118 11.8536 8.14645C12.0488 8.34171 12.0488 8.65829 11.8536 8.85355L7.85355 12.8536C7.75979 12.9473 7.63261 13 7.5 13C7.36739 13 7.24021 12.9473 7.14645 12.8536L3.14645 8.85355C2.95118 8.65829 2.95118 8.34171 3.14645 8.14645C3.34171 7.95118 3.65829 7.95118 3.85355 8.14645L7 11.2929L7 2.5C7 2.22386 7.22386 2 7.5 2Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg></div>
+                        </button>
+
                     </a>
                     <a
                         href="https://github.com/Nicogg-dev"
@@ -98,10 +103,14 @@ export const BentoGrid = () => {
                         className="flex flex-row text-black items-center gap-1"
                         rel="noreferrer"
                     >
-                        <FaGithub
-                            className="text-5xl md:text-6xl"
-                            style={{ color: "black" }}
-                        />
+                        <button className="group relative inline-flex h-14 md:h-16 items-center justify-center overflow-hidden rounded-full bg-neutral-950 p-1 font-medium text-neutral-200 duration-500">
+                            <div className="translate-y-0 opacity-100 transition group-hover:-translate-y-[150%] group-hover:opacity-0">
+                                <FaGithub
+                                    className="text-5xl md:text-6xl"
+                                />
+                            </div>
+                            <div className="absolute translate-y-[150%] opacity-0 transition group-hover:translate-y-0 group-hover:opacity-100"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6"><path d="M7.5 2C7.77614 2 8 2.22386 8 2.5L8 11.2929L11.1464 8.14645C11.3417 7.95118 11.6583 7.95118 11.8536 8.14645C12.0488 8.34171 12.0488 8.65829 11.8536 8.85355L7.85355 12.8536C7.75979 12.9473 7.63261 13 7.5 13C7.36739 13 7.24021 12.9473 7.14645 12.8536L3.14645 8.85355C2.95118 8.65829 2.95118 8.34171 3.14645 8.14645C3.34171 7.95118 3.65829 7.95118 3.85355 8.14645L7 11.2929L7 2.5C7 2.22386 7.22386 2 7.5 2Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg></div>
+                        </button>
                     </a>
                 </div>
             </div>
